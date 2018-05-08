2ad946c5895b92a87f6b2a7815276bed8c3f3c1a
mzq <www4836@gmail.com>
2018-05-08 13:22:30 +0800

    test

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.amazingcode.site']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
